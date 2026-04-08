198a0e3861301dfa44417d3f5bd46da495c30b62
Juan Yu <sigrace0417@gmail.com>
2026-04-08 22:56:14 +0300

    task2

---
+++ list/homework2.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class homework2{
+    public static void main(String[] args){
+        List<Integer> number = new ArrayList<>();
+        number.add(10);
+        number.add(20);
+        System.out.println(number);
+    }
+}
