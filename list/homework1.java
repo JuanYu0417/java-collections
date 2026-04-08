d7e21932721cfc0e2a9de3dabcaee2905e3367b5
Juan Yu <sigrace0417@gmail.com>
2026-04-08 22:53:32 +0300

    task1

---
+++ list/homework1.java
@@ -0,0 +1,10 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class homework1{
+    public static void main(String[] args){
+        List<String> name = new ArrayList<>();
+        name.add("Omena");
+        System.out.println(name);
+    }
+}
