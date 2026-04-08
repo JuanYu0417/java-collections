f46a58af5635c2b1cfee8a5eb60d5da6a5fffda6
Juan Yu <sigrace0417@gmail.com>
2026-04-08 23:14:22 +0300

    task5

---
+++ list/homework5.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class homework5{
+    public static void main(String[] args){
+        List<String> letterList = new ArrayList<>();
+        letterList.add("A");
+        letterList.add("B");
+        letterList.add("C");
+        System.out.println(letterList.size());
+        
+    }
+}
