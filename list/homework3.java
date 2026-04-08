014d945f2e903957c17137c684cb20e0ac2fb48f
Juan Yu <sigrace0417@gmail.com>
2026-04-08 23:03:59 +0300

    task3

---
+++ list/homework3.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class homework3{
+    public static void main(String[] args){
+        List<String> animaList = new ArrayList<>();
+        animaList.add("Kissa");
+        animaList.add("Koira");
+        System.out.println(animaList.get(0));
+    }
+}
