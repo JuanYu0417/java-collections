bfb9129ba54563f998886510b610d98c89502704
Juan Yu <sigrace0417@gmail.com>
2026-04-09 11:19:30 +0300

    task3

---
+++ set/homework3.java
@@ -0,0 +1,12 @@
+
+import java.util.TreeSet;
+
+public class homework3{
+    public static void main(String[] args){
+        TreeSet<Integer> set = new TreeSet<>();
+        set.add(3);
+        set.add(1);
+        set.add(2);
+        System.out.println(set);
+    }
+}
