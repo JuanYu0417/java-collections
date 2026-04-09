0452fe7afb21bbc28fa7f0bf249d36be7c999330
Juan Yu <sigrace0417@gmail.com>
2026-04-09 11:15:32 +0300

    task2

---
+++ set/homework2.java
@@ -0,0 +1,14 @@
+import java.util.Set;
+import java.util.HashSet;
+
+public class homework2{
+    public static void main(String[] args){
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        set.add(4)
+;       System.out.println(set);
+    }
+}
