7f8ba5f823d8dd62557a6b2dd9257d53cd7ce112
Juan Yu <sigrace0417@gmail.com>
2026-04-15 22:56:10 +0300

    C4

---
+++ combination/C4.java
@@ -0,0 +1,23 @@
+package combination;
+
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+
+public class C4 {
+    public static void main(String[] args){
+       System.out.println("Enter a sentence: ");
+       Map<String,Integer> word=new HashMap<>();
+       Scanner sc= new Scanner(System.in);
+       String sentence=sc.nextLine();
+       String[]words=sentence.split("\\s+");
+
+       for(String w:words){
+        String lowCaseWords = w.toLowerCase().replaceAll("[^a-z]","");
+           word.put(lowCaseWords, word.getOrDefault(lowCaseWords, 0) + 1);
+       }
+       System.out.println(word + ", " + words.length);
+       sc.close();A 
+    }
+
+}
